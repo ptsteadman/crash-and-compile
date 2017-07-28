9bb01f2661f95505139e7aeb14da208e1a27bbe3
Patrick Steadman <ptsteadman@gmail.com>
2017-07-28 14:26:48 -0700

    PoC

--- 25-quals/locate/easy.py
+++ 25-quals/locate/easy.py
@@ -12,12 +12,32 @@ except EOFError:
 
 pass
 
+X_LETTERS = ['A', 'B', 'C', 'D', 'E']
+
 boards = boards_str.split("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
 for board in boards:
     images = board.split("\n")
+    pil_images = []
     for image in images:
         if image:
             image_bytes = base64.b64decode(image)
             i = Image.frombytes('RGB', (5, 5), image_bytes)
-            print(i)
+            pil_images.append(i)
+
+    for x in range(0,5):
+        for y in range(0,5):
+            redshift = True
+            red_high = 0
+            for pi in pil_images:
+                r, g, b = pi.getpixel((x, y))
+                if r > red_high:
+                    red_high = r
+                else:
+                    redshift = False
+                    red_high = r # necessary?
+            if redshift:
+                print('{0}{1}'.format(X_LETTERS[x], y))
+
+
+
 
