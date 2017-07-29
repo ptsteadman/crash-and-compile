3c92511e5429d202729248a780608c5f2881b1f3
Patrick Steadman <ptsteadman@gmail.com>
2017-07-28 17:29:29 -0700

    working easy

--- 25-quals/locate/easy.py
+++ 25-quals/locate/easy.py
@@ -15,36 +15,33 @@ pass
 
 X_LETTERS = ['A', 'B', 'C', 'D', 'E']
 
+def get_quad_tuple(x, y, size):
+    left_x = (size * x) + (x + 1)
+    right_x = left_x + size
+    top_y = (size * y) + (y + 1)
+    bottom_y = top_y + size
+    return (left_x, top_y, right_x, bottom_y)
+
+def has_redshift(quad):
+    for quad_x in range(0, 9):
+        for quad_y in range(0, 9):
+            rgb_quad = quad.convert('RGB')
+            colors = rgb_quad.getpixel((quad_x, quad_y))
+            if colors == (255, 0, 0):
+                return True
+    return False
+
 boards = boards_str.split("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
 for board in boards:
-    images = board.split("\n")
-    for i, image in enumerate(images):
-        if image:
-            image_bytes = base64.b64decode(image)
-            buf = io.BytesIO(image_bytes)
-            pi = Image.open(buf)
-            # pi = Image.frombytes('RGB', (5, 5), image_bytes)
-            pi.save('{0}.bmp'.format(str(i)))
+    images = list(filter(None, board.split("\n")))
 
     for x in range(0,5):
         for y in range(0,5):
             # print('{0}{1}'.format(X_LETTERS[x], y + 1))
-            redshift = True
-            red_high = 0
-            for image in images:
-                if image:
-                    image_bytes = base64.b64decode(image)
-                    buf = io.BytesIO(image_bytes)
-                    pi = Image.open(buf)
-                    # pi = Image.frombytes('RGB', (5, 5), image_bytes)
-                    pi.save('{0}{1}.bmp'.format(X_LETTERS[x], y + 1))
-                    rgb = pi.convert('RGB')
-                    r, g, b = rgb.getpixel((x, y))
-                    # print('{0} {1} {2}'.format(r,g,b))
-                    # print('{0} {1}'.format(red_high, r))
-                    if r > red_high:
-                        red_high = r
-                    else:
-                        redshift = False
-            if redshift:
-                print('{0}{1}'.format(X_LETTERS[x], y + 1))
+            for i, image in enumerate(images):
+                image_bytes = base64.b64decode(image)
+                buf = io.BytesIO(image_bytes)
+                pil_i = Image.open(buf)
+                quad = pil_i.crop(get_quad_tuple(x, y, 9))
+                if has_redshift(quad):
+                    print('{0}{1}'.format(X_LETTERS[x], y + 1))
